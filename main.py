44c9198c48d6e2fe4d3fa780649e654ca6feb923
revanth11 <revanthk486@gamil.com>
2025-02-25 19:41:18 +0530

    model trainer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.state_01_data_ingestion import DataIngestionTrainingPipeLine
 from mlProject.pipeline.state_02_data_validation import DataValidationTrainingPipeLine
 from mlProject.pipeline.state_03_data_transformation import DataTransformationTrainingPipeLine
+from mlProject.pipeline.state_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -38,3 +39,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Model trainer stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<< \n\n X=========X')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
